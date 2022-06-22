074676c2e7075e57334b4fd7bb89e50dfed31cf1
andre-bandeli <andre.lbandeli@gmail.com>
2022-06-22 05:04:22 -0300

    feat: kafka configs

--- add-music/src/main/java/com/br/kafka/spring/boot/addmusic/AddMusicApplication.java
+++ add-music/src/main/java/com/br/kafka/spring/boot/addmusic/AddMusicApplication.java
@@ -2,8 +2,12 @@ package com.br.kafka.spring.boot.addmusic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableKafka
+@EnableAsync
 public class AddMusicApplication {
 
 	public static void main(String[] args) {
