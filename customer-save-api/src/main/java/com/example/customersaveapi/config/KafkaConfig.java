0988ae60e80339906af932ab5f0b99a0fd041fc8
andre-bandeli <andre.lbandeli@gmail.com>
2022-07-02 17:35:07 -0300

    feat: criando api consumer save

---
+++ customer-save-api/src/main/java/com/example/customersaveapi/config/KafkaConfig.java
@@ -0,0 +1,110 @@
+package com.example.customersaveapi.config;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+
+    /*
+    * bootstrap. servers is a comma-separated list of host and port pairs that are the addresses of the
+    * Kafka brokers in a "bootstrap" Kafka cluster that a Kafka client connects to initially to bootstrap itself.
+    * */
+    @Value("127.0.0.1:9092")
+    private String bootstrapServers;
+    @Value("${kafka.topic.requestreply-topic}")
+    private String requestReplyTopic;
+    @Value("${kafka.consumergroup}")
+    private String consumerGroup;
+
+    /*
+    * by medium.com: os objetos que formam a espinha dorsal da sua
+    * aplicação e que sejam gerenciados pelo Spring são chamados de beans.
+    * Um bean é um objeto que é instanciado, montado e gerenciado pelo Spring IoC container
+    * https://medium.com/@decioluckow/explorando-bean-spring-ioc-e640c53d29a9
+    * */
+    @Bean
+    public Map<String, Object> producerConfigs() {
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+
+        return props;
+    }
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,
+                consumerGroup);
+
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ReplyingKafkaTemplate<String, String, String> replyingKafkaTemplate(ProducerFactory<String, String> producerFactory,
+                                                                               KafkaMessageListenerContainer kafkaMessageListenerContainer) {
+        ReplyingKafkaTemplate template = new ReplyingKafkaTemplate<>(producerFactory, kafkaMessageListenerContainer);
+        template.setDefaultReplyTimeout(Duration.ofDays(60000));
+
+        return template;
+
+    }
+
+    @Bean
+    public KafkaMessageListenerContainer<String, String> replyContainer(ConsumerFactory<String, String> consumerFactory) {
+        ContainerProperties containerProperties = new ContainerProperties(requestReplyTopic);
+        return new KafkaMessageListenerContainer<>(consumerFactory, containerProperties);
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(), new StringDeserializer());
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setReplyTemplate(kafkaTemplate());
+
+        return factory;
+    }
+
+
+}
