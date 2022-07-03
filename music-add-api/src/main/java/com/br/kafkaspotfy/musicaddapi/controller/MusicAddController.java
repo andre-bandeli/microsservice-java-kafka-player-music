ce972749de67e16023438b7a92d44b42c0f8876b
andre-bandeli <andre.lbandeli@gmail.com>
2022-07-03 08:13:31 -0300

    feat: criando api music

---
+++ music-add-api/src/main/java/com/br/kafkaspotfy/musicaddapi/controller/MusicAddController.java
@@ -0,0 +1,23 @@
+package com.br.kafkaspotfy.musicaddapi.controller;
+
+
+import com.br.kafkaspotfy.musicaddapi.model.MusicJson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class MusicAddController {
+
+    @Autowired
+    private SaveMusicService saveMusicService;
+
+    public String create(@PathVariable("uuid") String uuid, @RequestBody MusicJson customerJson) {
+
+        customerJson.setUuidCustomer(uuid);
+        return saveMusicService.execute(customerJson);
+    }
+}
