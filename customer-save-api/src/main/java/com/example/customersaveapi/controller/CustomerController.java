ac471b78df915a86b5934ddb3b2c099990b00a6d
andre-bandeli <andre.lbandeli@gmail.com>
2022-07-02 23:26:02 -0300

    feat: implementando service + controller + model

---
+++ customer-save-api/src/main/java/com/example/customersaveapi/controller/CustomerController.java
@@ -0,0 +1,24 @@
+package com.example.customersaveapi.controller;
+
+import com.example.customersaveapi.model.CustomerJson;
+import com.example.customersaveapi.service.SaveCustomerService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping("/v1")
+public class CustomerController {
+
+    @Autowired
+    private SaveCustomerService saveCustomerService;
+
+    @PostMapping("/")
+    public String create(@RequestBody CustomerJson customerJson) throws ExecutionException, JsonProcessingException, InterruptedException {
+        return saveCustomerService.execute(customerJson);
+    }
+
+
+}
---
+++ customer-save-api/src/main/java/com/example/customersaveapi/model/CustomerJson.java
@@ -0,0 +1,61 @@
+package com.example.customersaveapi.model;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.NotNull;
+
+
+public class CustomerJson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private String uid;
+
+    @NotEmpty
+    @NotNull
+    private String name;
+
+    @NotEmpty
+    @NotNull
+    private String country;
+
+    @NotEmpty
+    @NotNull
+    private String musicStyle;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getMusicStyle() {
+        return musicStyle;
+    }
+
+    public void setMusicStyle(String musicStyle) {
+        this.musicStyle = musicStyle;
+    }
+}
+
