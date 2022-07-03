79dc19e433ec7d3f11cea0128c8b7563ba9fe018
andre-bandeli <andre.lbandeli@gmail.com>
2022-07-03 08:24:04 -0300

    feat: implementando configs + criando api music service

--- music-add-api/src/main/java/com/br/kafkaspotfy/musicaddapi/controller/MusicAddController.java
+++ music-add-api/src/main/java/com/br/kafkaspotfy/musicaddapi/controller/MusicAddController.java
@@ -2,11 +2,15 @@ package com.br.kafkaspotfy.musicaddapi.controller;
 
 
 import com.br.kafkaspotfy.musicaddapi.model.MusicJson;
+import com.br.kafkaspotfy.musicaddapi.service.SaveMusicService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/v1")
@@ -15,7 +19,7 @@ public class MusicAddController {
     @Autowired
     private SaveMusicService saveMusicService;
 
-    public String create(@PathVariable("uuid") String uuid, @RequestBody MusicJson customerJson) {
+    public String create(@PathVariable("uuid") String uuid, @RequestBody MusicJson customerJson) throws ExecutionException, JsonProcessingException, InterruptedException {
 
         customerJson.setUuidCustomer(uuid);
         return saveMusicService.execute(customerJson);
